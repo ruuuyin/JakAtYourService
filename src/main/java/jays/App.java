6f6c18f2b2aa20e921597234a489b4e71d2aba90
legion <mstream64@gmail.com>
2020-05-04 11:41:08 +0800

    Login UI and creating css standard for UI controls

--- src/main/java/jays/App.java
+++ src/main/java/jays/App.java
@@ -1,17 +1,21 @@
 package jays;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class App extends Application {
 
-    private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-
+        Parent parent = FXMLLoader.load(getClass().getResource("/fxml/JLogin.fxml"));
+        stage.setScene(new Scene(parent));
+        stage.getScene().getStylesheets().add(String.valueOf(getClass().getResource("/style/LightMode.css")));
+        stage.show();
     }
 
 
--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module jays {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens jays to javafx.fxml;
-    exports jays;
-}

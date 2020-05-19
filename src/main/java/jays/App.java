3df64ac25d68080f000d49243496f06299cb9b29
legion <mstream64@gmail.com>
2020-05-19 22:38:02 +0800

    Basic component of JServices.fxml added and including the DarkMode.css on every design

--- src/main/java/jays/App.java
+++ src/main/java/jays/App.java
@@ -12,9 +12,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        Parent parent = FXMLLoader.load(getClass().getResource("/fxml/JLogin.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("/fxml/JServices.fxml"));
         stage.setScene(new Scene(parent));
-        stage.getScene().getStylesheets().add(String.valueOf(getClass().getResource("/style/LightMode.css")));
+        stage.getScene().getStylesheets().add(String.valueOf(getClass().getResource("/style/DarkMode.css")));
         stage.show();
     }
 
